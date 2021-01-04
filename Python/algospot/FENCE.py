e37ab32f3131abe6f5f56fd849ca4a338461d201
madeline <zzzz465@naver.com>
2021-01-04 18:05:55 +0900

    파이썬이 느린건가?

--- Python/algospot/FENCE.py
+++ Python/algospot/FENCE.py
@@ -1,32 +1,43 @@
+import os, sys
+
 C = int(input()) # 테스트 케이스
 
-# 아이디어
-'''
-N개의 판자 중에서 가장 큰 사각형을 구하는 방법은
-N-1 개의 판자 중에서 가장 큰 사각형과 N개의 판자를 사각형으로 만들었을 때
-더 큰 것을 선택하면 된다.
-N-1개의 판자보다 N개의 판자로 만드는 사각형이 더 크다면, N-2보다는 당연히 더 커질 것이므로
-'''
+sys.setrecursionlimit(100000)
 
 for _ in range(C):
     N = int(input()) # 판자 수
     planks = list(map(int, input().split())) # 판자 개수는 1~10000개, 각각의 높이 범위는 1~10000개 이다
 
     def getMax(start, end): # start: int, end: int
-        if start < end:
-            length = end - start
+        # start과 end는 index, 0 ~ len(planks) - 1
+        if start == end:
+            return planks[start]
 
-            if length == 1: # 기저 사례: 더이상 쪼갤 수 없으므로 판자 그 자체가 하나의 사각형임
-                return planks[start]
+        mid = int((start + end) / 2)
 
-            max_1 = min(planks[start:start + length]) * length
-            max_2 = getMax(start, end - 1)
-            max_3 = getMax(start + 1, end)
+        max_left = getMax(start, mid) # 왼쪽에 있을 경우
+        max_right = getMax(mid + 1, end) # 오른쪽에 있을 경우
 
-            return max(max_1, max_2, max_3)
-        else:
-            return 0
+        max_size = max(max_left, max_right)
 
-    max_size = getMax(0, len(planks))
+        low = mid
+        high = mid + 1
+        height = min(planks[low:high + 1])
+
+        max_size = max(max_size, height * 2)
+
+        while start < low or high < end:
+            if high < end and (low == start or planks[low - 1] < planks[high + 1]):
+                high += 1
+                height = min(height, planks[high])
+            else:
+                low -= 1
+                height = min(height, planks[low])
+
+            max_size = max(max_size, height * (high - low + 1))
+
+        return max_size
+
+    max_size = getMax(0, len(planks) - 1)
 
     print(max_size)
