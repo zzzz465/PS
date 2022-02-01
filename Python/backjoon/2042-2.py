f917035e33fb7ad6c0e88700a3f7d885fd2315cd
do.kim <do.kim@pubg.com>
2022-02-02 00:28:41 +0900

    February 2, 2022 12:28 AM

--- Python/backjoon/2042-2.py
+++ Python/backjoon/2042-2.py
@@ -6,3 +6,31 @@ while True:
         break
     else:
         tree_height += 1
+
+tree_nodes_count = 2 ** (tree_height) + 1  # starts from 1
+
+tree = [0] * tree_nodes_count
+
+
+def update(start: int, end: int, node: int, index: int, value: int):
+    if start == end:
+        tree[node] = value
+
+    mid = (start + end) // 2
+
+    if mid <= index:
+        return update(start, mid, node * 2, index, value)
+    else:
+        return update(mid + 1, end, node * 2 + 1, index, value)
+
+
+def query(start: int, end: int, node: int, range_start: int, range_end: int):
+    if start == range_start and end == range_end:
+        return tree[node]
+
+    mid = (start + end) // 2
+
+    left = query(start, mid, node * 2, range_start, mid)
+    right = query(mid + 1, end, node * 2 + 1, mid + 1, end)
+
+    return left + right
