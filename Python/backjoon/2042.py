c1d73b077d017494d4bb6912c5bdfd5a04989720
do.kim <do.kim@pubg.com>
2022-02-01 17:24:01 +0900

    February 1, 2022 5:24 PM

--- Python/backjoon/2042.py
+++ Python/backjoon/2042.py
@@ -1,3 +1,6 @@
+import heapq
+
+
 N, M, K = map(int, input().split())
 
 '''
@@ -5,8 +8,22 @@ N: 수의 개수
 M: 변경 횟수
 K: 구간의 합을 구하는 횟수
 
-B+ tree 를 구하면 될 것 같지 않은가?
+B+ tree 를 구현하면 될 것 같지 않은가?
 '''
+
+tree = []
+
+def leaf_depth():
+    _N = N
+    count = 0
+    while _N > 0:
+        _N = _N // 2
+        count += 1
+    pass
+
+def insert(index: int, value: int):
+
+    pass
 
 for i in range(N):
     int(input())
