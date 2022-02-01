e54a7a90373a6aae2659f6ab6beceeae011f78e0
do.kim <do.kim@pubg.com>
2022-02-02 01:00:48 +0900

    February 2, 2022 1:00 AM

--- Python/backjoon/2042-2.py
+++ Python/backjoon/2042-2.py
@@ -1,27 +1,34 @@
 N, K, M = map(int, input().split())
 
-tree_height = 1
-while True:
-    if 2 ** (tree_height + 1) > N:
-        break
-    else:
-        tree_height += 1
+leaf_nodes_count = 1
+while leaf_nodes_count < N:
+    leaf_nodes_count *= 2
 
-tree_nodes_count = 2 ** (tree_height) + 1  # starts from 1
+tree_nodes_count = leaf_nodes_count - 1
 
-tree = [0] * tree_nodes_count
+
+tree = [0] * (tree_nodes_count + 1)
 
 
 def update(start: int, end: int, node: int, index: int, value: int):
     if start == end:
         tree[node] = value
+        return value
 
     mid = (start + end) // 2
 
-    if mid <= index:
-        return update(start, mid, node * 2, index, value)
+    left, right = 0, 0
+
+    if index <= mid:
+        left = update(start, mid, node * 2, index, value)
+        right = query(mid + 1, end, node * 2 + 1, mid + 1, end)
+        tree[node] = left + right
     else:
-        return update(mid + 1, end, node * 2 + 1, index, value)
+        left = query(start, mid, node * 2, start, mid)
+        right = update(mid + 1, end, node * 2 + 1, index, value)
+        tree[node] = left + right
+
+    return left + right
 
 
 def query(start: int, end: int, node: int, range_start: int, range_end: int):
@@ -37,3 +44,18 @@ def query(start: int, end: int, node: int, range_start: int, range_end: int):
     right = query(mid + 1, end, node * 2 + 1, mid + 1, end)
 
     return left + right
+
+
+for i in range(1, N + 1):
+    val = int(input())
+    update(1, leaf_nodes_count, 1, i, val)
+
+
+for _ in range(K + M):
+    a, b, c = map(int, input().split())
+
+    if a == 1:
+        update(1, leaf_nodes_count, 1, b, c)
+    else:
+        res = query(1, leaf_nodes_count, 1, b, c)
+        print(res)
