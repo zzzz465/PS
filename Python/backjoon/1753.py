2f27ab63e1cb06608d4475e5e9e0007ab511bc2b
do.kim <do.kim@pubg.com>
2022-02-01 16:02:28 +0900

    February 1, 2022 4:02 PM

--- Python/backjoon/1753.py
+++ Python/backjoon/1753.py
@@ -1,4 +1,5 @@
 from collections import defaultdict, deque
+import heapq
 import sys
 from typing import Dict, List, Set, Tuple
 
@@ -20,22 +21,25 @@ vertices: List[int] = [sys.maxsize for _ in range(V + 1)]
 vertices[K] = 0
 visited: Set[int] = set()
 
-q = deque()
-q.append(K)
+hq = []
+heapq.heappush(hq, (0, K))
 
-while len(q) > 0:
-    u = q.popleft()
+while len(hq) > 0:
+    _, u = hq.popleft()
 
     if u in visited:
         continue
+    else:
+        visited.add(u)
 
     vwArr = edges[u]
 
     for (v, w) in vwArr:
         vertices[v] = max(vertices[v], vertices[u] + w)
+        heapq.heappush(hq, (vertices[v], v))
 
 for i in range(1, V + 1):
     if vertices[i] == sys.maxsize:
         print('INF')
     else:
-        print()
+        print(vertices[i])
