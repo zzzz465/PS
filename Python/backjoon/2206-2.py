94910961ebd9b202b4fcf73ac093d65a449a4d40
do.kim <do.kim@pubg.com>
2022-02-01 16:37:34 +0900

    February 1, 2022 4:37 PM

--- Python/backjoon/2206-2.py
+++ Python/backjoon/2206-2.py
@@ -11,18 +11,47 @@ INVALID_TILE = 1
 # 좌측 상단 (1, 1) 에서 우측 하단 (N, N) 으로 도달하는 최소한의 거리
 
 from collections import deque
+import sys
 from typing import Set, Tuple
 
 # Tuple<y: int, x: int, score: int, pickaxe_used: bool>
 queue = deque()
 visited: Set[Tuple[int, int]] = set()
 
-queue.append(())
+queue.append((0, 0, 0, False))
 
-def visit(y: int, x: int, score: )
+def visit(y: int, x: int, score: int, pickaxe_used: bool):
+    # is valid?
+    if y < 0 or y >= N or x < 0 or x >= M:
+        return
+
+    if arr[y][x] == INVALID_TILE and not pickaxe_used:
+        queue.append((y, x, score + 1, True))
+
+    queue.append((y, x, score + 1, True))
+
+max_score = -sys.maxsize
 
 while len(queue) > 0:
-    score, breaked, y, x = queue.popleft()
+    y, x, score, pickaxe_used = queue.popleft()
 
-    if (y, x) in visited:
+    if y == N - 1 and x == M - 1:
+        max_score = max(max_score, score)
         continue
+
+    # up
+    visit(y - 1, x, score, pickaxe_used)
+    
+    # right
+    visit(y, x + 1, score, pickaxe_used)
+
+    # down
+    visit(y + 1, x, score, pickaxe_used)
+
+    # left
+    visit(y, x - 1, score, pickaxe_used)
+
+if max_score == -sys.maxsize:
+    print(-1)
+else:
+    print(max_score)
