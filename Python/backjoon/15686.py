322acb63c5c77eb83f033aff3f62b52131b501a0
madeline <zzzz465@naver.com>
2022-05-19 10:59:40 +0900

    May 19, 2022 10:59 AM

--- Python/backjoon/15686.py
+++ Python/backjoon/15686.py
@@ -21,3 +21,9 @@ for y, li in enumerate(mat):
         elif val == HOUSE:
             houses.append(p)
 
+
+def dist(p1: Point, p2: Point) -> int:
+    d1 = abs(p1[0] - p2[0])
+    d2 = abs(p1[1] - p2[1])
+
+    return d1 + d2
