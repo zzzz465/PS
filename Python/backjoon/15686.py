5ee277577458ef40c770f7edf8c8382d75035242
madeline <zzzz465@naver.com>
2022-05-19 10:48:36 +0900

    May 19, 2022 10:48 AM

--- Python/backjoon/15686.py
+++ Python/backjoon/15686.py
@@ -5,9 +5,19 @@ N, M = map(int, input().split())
 
 mat = [*map([*map(int, input().split())], range(N))]
 
-chickens: List[Tuple[int, int]] = []
-houses: List[Tuple[int, int]] = []
+Point = Tuple[int, int]
+
+CHICKEN = 2
+HOUSE = 1
+
+chickens: List[Point] = []
+houses: List[Point] = []
 
 for y, li in enumerate(mat):
     for x, val in enumerate(li):
-        pass
+        p: Point = (y, x)
+        if val == CHICKEN:
+            chickens.append(p)
+        elif val == HOUSE:
+            houses.append(p)
+
