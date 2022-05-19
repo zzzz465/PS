58706637092db7864e744d8b2e59122e5ccf3c9d
madeline <zzzz465@naver.com>
2022-05-19 10:45:39 +0900

    May 19, 2022 10:45 AM

--- Python/backjoon/15686.py
+++ Python/backjoon/15686.py
@@ -1,9 +1,13 @@
+from typing import List, Tuple
+
+
 N, M = map(int, input().split())
 
 mat = [*map([*map(int, input().split())], range(N))]
 
+chickens: List[Tuple[int, int]] = []
+houses: List[Tuple[int, int]] = []
+
 for y, li in enumerate(mat):
     for x, val in enumerate(li):
         pass
-
-
