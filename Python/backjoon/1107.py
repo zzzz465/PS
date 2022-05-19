ed4e4d341236a9553af3a7a46a184ae5e6408584
madeline <zzzz465@naver.com>
2022-05-19 21:32:05 +0900

    May 19, 2022 9:32 PM

--- Python/backjoon/1107.py
+++ Python/backjoon/1107.py
@@ -1,5 +1,7 @@
 N = int(input())
 M = int(input())
 broken_buttons = list(map(int, input().split()))
+available_buttons = sorted(
+    {1, 2, 3, 4, 5, 6, 7, 8, 9, 0} - set(broken_buttons))
 
 # 1. 사용 가능한 수로 N이랑 최대한 가까운 수 만들기
